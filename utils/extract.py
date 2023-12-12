2c41df633fea0f2054c828086989e7e8f0a15e20
Ouuumar <sk8pro@hotmail.fr>
2023-12-12 10:21:50 +0100

    refacto: refacto writing data to disk

--- utils/extract.py
+++ utils/extract.py
@@ -5,6 +5,9 @@ import logging
 
 from pyspark.sql import DataFrame
 
+from utils.processing import is_containing_nulls
+
+
 logging.basicConfig(filename="./logs/logs.log", format="%(name)s - %(levelname)s - %(asctime)s - %(message)s", level=logging.INFO)
 
 
@@ -16,7 +19,7 @@ def extract_data(params:dict) -> list:
     - params (dict): Query parameters for the API request.
 
     Returns:
-    list: A list of json containing the extracted data.
+    list (List[dict]): A list of json containing the extracted data.
     """
     try:
         logging.info("Starting requesting data to API...")
@@ -26,23 +29,53 @@ def extract_data(params:dict) -> list:
         logging.error("Problem requesting data...", e)
 
 
-def write_data(data:json or DataFrame, level:str):
+def write_bronze(data:json) -> None:
+    """
+    Write data to a bronze-level file.
+
+    Parameters:
+    - data (json): The data to be written, typically raw JSON data.
+    """
+    try:
+        with open(f"./data/bronze/{datetime.today().strftime('%Y-%m-%d')}_data.json", 'x') as f:
+            json.dump(data, f)
+        logging.info("Bronze data written successfully.")
+    except Exception as e:
+        logging.error("Problem writing bronze data...", e)
+        
+
+def write_silver(data:DataFrame, date:str) -> None:
+    """
+    Write data to a silver-level file.
+
+    Parameters:
+    - data (pyspark.sql.DataFrame): The PySpark DataFrame containing processed data to be written.
+    - date (str): The date parameter for the output file (optional).
+    """
+    try:
+        if date:
+            data.write.mode("overwrite").parquet(f"./data/silver/{date}_data.parquet")
+        else:
+            data.write.mode("overwrite").parquet(f"./data/silver/{datetime.today().strftime('%Y-%m-%d')}_data.parquet")
+        logging.info("Silver data written successfully.")
+    except Exception as e:
+        logging.error("Problem writing silver data...", e)
+
+
+def write_data(data:DataFrame or json, level:str, date:str=None) -> None:
     """
     Writes data to disk.
 
     Parameters:
     - data (json or DataFrame): The raw JSON data from the API or a PySpark DataFrame.
     - level (str): The processing level for writing data, either "bronze" or "silver".
+    - date (str): The date of a particular JSON file to write (optional).
     """
     logging.info("Starting writing data to local...")
+
     if level == "bronze":
-        try:
-            with open(f"./data/{level}/{datetime.today().strftime('%Y-%m-%d')}_data.json", 'x') as f:
-                json.dump(data, f)
-        except Exception as e:
-            logging.error("Problem writing data...", e)
-    elif level == "silver":
-        data.write.mode("overwrite").parquet(f"./data/{level}/{datetime.today().strftime('%Y-%m-%d')}_data.parquet") 
-
-
-    
+        write_bronze(data)
+    elif level == "silver" and not is_containing_nulls(data):
+        write_silver(data, date)
+    else:
+        logging.warning("Data not written due to level or null values.")
